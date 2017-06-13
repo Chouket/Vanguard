8e341166ff07c3ee2a3eee60ea66e8a3d556b80b
Aubart Thomas <t.aubart@student.isartdigital.com>
2017-06-13 10:09:27 +0200

    Try to clean

---
+++ VanguardProject/Assets/Script/Manager/NetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    static private NetworkManager instance = null;
+    static public NetworkManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new NetworkManager();
+            return instance;
+        }
+    }
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+    }
+}
---
+++ VanguardProject/Assets/Script/Manager/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    static private PlayerManager instance = null;
+    static public PlayerManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new PlayerManager();
+            return instance;
+        }
+    }
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+	}
+}
