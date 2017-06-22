546c81013003f6fdb41a4b57480899c863ad0804
Louis Schmidt <l.schmidt@student.isartdigital.com>
2017-06-22 09:42:53 +0200

    add plugin Json.net, start of JsonParser
    Extract data from json WIP (actually get error, wait Deus to fix it)

---
+++ VanguardOnline/Assets/Script/Tools/JsonCardData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.Tools
+{
+    class JsonCardData
+    {
+        public int CardID;
+        public string Name;
+        public int Grade;
+        public int ATK;
+        public string Clan;
+        public string Race;
+        public string Nation;
+        public string Type;
+        public string Text;
+    }
+}
